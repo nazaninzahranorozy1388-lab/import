db4406eb218d0a05dd5b33e6e1fd7f2ff6d426cc
nazaninzahranorozy1388-lab <nazaninzahranorozy1388@gmail.com>
2025-10-04 16:04:38 +0330

    Add files via upload

---
+++ masahat.py
@@ -0,0 +1,37 @@
+#مساحت مستطیل
+#def rectangle_area(length,width):
+ #   area = length * width
+ #   return area
+#v = rectangle_area(10,20)
+#print(v)
+import math
+
+
+#محیط مستطیل
+#def rectangle_environment(length,width):
+#    area = length * 2 + width * 2
+ #   return area
+#s = int(input("Enter the length :"))
+#a = int(input("Enter the width :"))
+#v = rectangle_environment(s,a)
+#print(v)
+
+#مساحت دایره
+#def circle_area(radius):
+ #   return radius * radius * 3.14
+
+#v = circle_area(3)
+#print(v)
+
+#محیط دایره
+def circle_environment(radius):
+    area = 2 * 3.14 * radius
+    return area
+
+s = int(input("Enter the redius :"))
+v = circle_environment(s)
+print(v)
+
+
+
+
